cba758a3ff7c9e3f9a67fae4b2b1b6d9122d480b
jessevaughan <jesse.s.vaughan@gmail.com>
2026-04-26 11:11:15 -0700

    auto prettify!!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://www.jessevaughan.com',
+  site: "https://www.jessevaughan.com",
   integrations: [sitemap()],
 });
